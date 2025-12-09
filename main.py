f1314e2b67c94beebf4e065b150076c905272f6f
Bowlee <ldoh0321@gmail.com>
2025-12-09 16:49:38 +0900

    update main.py

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ second = int(input("enter the second number: "))
 sum_result = first + second
 sub_result = first - second
 prod_result = first * second
+
+print("Sum: " + sum_result)
+print("Sub: " + sub_result)
+print("Prod: " + prod_result)
+
+
